f7e689a28fb5f7b4efef8d06b05cd6f43eaa04b6
Oroko-bilhah <orokobilhah@gmail.com>
2023-08-24 23:03:29 -0700

    betty

---
+++ betty.c
@@ -0,0 +1,15 @@
+/**
+ * Return: Always returns 0
+ */
+int main(void)
+{
+	int i; /* Counter variable */
+
+	for (i = 0; i < 5; i++)
+	{
+		/* Print the value of i */
+		printf("i = %d\n", i);
+	}
+
+	return (0);
+}
